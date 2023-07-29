3afd4397b1786f11bb5b8ebab6ce4ec1620bd9f1
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-07-29 23:31:13 +1000

    Uncaught reference error False is not defined at Script.js:8

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const password2El = document.getElementById('passworld2');
 const containerMessage = document.querySelector('.container-mssage');
 const message = document.getElementById('message');
 
+let isValid = fales; //Default value for validateForm function
+
+function validateForm(){
+    //Use Constraint API
+    isValid = form.checkValidity();
+    console.log(isValid);//For testing initial form validation
+}
+
 function processForms(e){
     e.preventDefault();// prevent from refreshing form data
-    //Testing: console.log(e);
+    //For test: console.log(e);
     // Validate form
     validateForm();
     // Store data
