3fc992b57931a834ee7d3cb560c583505dc8825c
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-07-29 22:45:19 +1000

    Complete creating const variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const form ="";
-const password1El = "";
-const password2El = "";
-const messageContainer ="";
-const message ="";
+const form = document.getElementById('form');
+const password1El = document.getElementById('passworld1');
+const password2El = document.getElementById('passworld2');
+const containerMessage = document.querySelector('.container-mssage');
+const message = document.getElementById('message');
