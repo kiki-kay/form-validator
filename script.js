19b9148eabb00f650851e5747073f316cecd145f
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-07-29 22:55:32 +1000

    Add Event Listener to function to process forms

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// The main variables
 const form = document.getElementById('form');
 const password1El = document.getElementById('passworld1');
 const password2El = document.getElementById('passworld2');
 const containerMessage = document.querySelector('.container-mssage');
 const message = document.getElementById('message');
+
+// Event Listener
+form.addEventListener('submit', functionToProcessForms);
